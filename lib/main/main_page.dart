b64a2e7c04906491ac449d9a40d7eb6143acd49a
jojo18noor <jo2014saleh@gmail.com>
2021-11-02 14:50:17 +0300

    animation botton nav bar

--- lib/main/main_page.dart
+++ lib/main/main_page.dart
@@ -7,6 +7,7 @@ import 'package:yanni_store/accuonts/signin_page.dart';
 import 'package:yanni_store/categories/category_page.dart';
 import 'package:yanni_store/home/home_page.dart';
 import 'package:yanni_store/store/store_page.dart';
+import 'botton_nav_bar.dart';
 import 'nav_controller.dart';
 
 class MainPage extends StatelessWidget {
@@ -17,63 +18,104 @@ class MainPage extends StatelessWidget {
     return GetBuilder<BottonNav>(
       builder: (controller) {
         return Scaffold(
-          // floatingActionButton: FloatingActionButton(
-          //   backgroundColor: Colors.blueGrey,
-          //   child: Icon(Icons.search),
-          //   onPressed: () {},
-          // ),
-          body: SafeArea(
-            child: IndexedStack(
-              index: controller.tabIndex,
-              children: [
-                HomePage(),
-                StorePage(),
-                CategoryPage(),
-                // SignInPage(),
-                CustomerSigninPage()
-              ],
-            ),
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            onTap: controller.onTab,
-            currentIndex: controller.tabIndex,
-            items: [
-              BottomNavigationBarItem(
-                backgroundColor: Colors.blue[900],
-                icon: Icon(
-                  Icons.home_outlined,
-                  color: Colors.white,
+            // floatingActionButton: FloatingActionButton(
+            //   backgroundColor: Colors.blueGrey,
+            //   child: Icon(Icons.search),
+            //   onPressed: () {},
+            // ),
+            body: SafeArea(child: _body()
+                // IndexedStack(
+                //   index: controller.tabIndex,
+                //   children: [
+                //     HomePage(),
+                //     StorePage(),
+                //     CategoryPage(),
+                //     CustomerSigninPage()
+                //   ],
+                // ),
                 ),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                backgroundColor: Colors.blue[900],
-                icon: Icon(
-                  Icons.shopping_cart_outlined,
-                  color: Colors.white,
-                ),
-                label: 'Store',
-              ),
-              BottomNavigationBarItem(
-                backgroundColor: Colors.blue[900],
-                icon: Icon(
-                  Icons.category_outlined,
-                  color: Colors.white,
-                ),
-                label: 'categories',
-              ),
-              BottomNavigationBarItem(
-                backgroundColor: Colors.blue[900],
-                icon: Icon(
-                  Icons.person_outline,
-                  color: Colors.white,
-                ),
-                label: 'Account',
-              ),
-            ],
-          ),
-        );
+            bottomNavigationBar: _bottonNavBar()
+            //  BottomNavigationBar(
+            //   onTap: controller.onTab,
+            //   currentIndex: controller.tabIndex,
+            //   items: [
+            //     BottomNavigationBarItem(
+            //       backgroundColor: Colors.blue[900],
+            //       icon: Icon(
+            //         Icons.home_outlined,
+            //         color: Colors.white,
+            //       ),
+            //       label: 'Home',
+            //     ),
+            //     BottomNavigationBarItem(
+            //       backgroundColor: Colors.blue[900],
+            //       icon: Icon(
+            //         Icons.shopping_cart_outlined,
+            //         color: Colors.white,
+            //       ),
+            //       label: 'Store',
+            //     ),
+            //     BottomNavigationBarItem(
+            //       backgroundColor: Colors.blue[900],
+            //       icon: Icon(
+            //         Icons.category_outlined,
+            //         color: Colors.white,
+            //       ),
+            //       label: 'categories',
+            //     ),
+            //     BottomNavigationBarItem(
+            //       backgroundColor: Colors.blue[900],
+            //       icon: Icon(
+            //         Icons.person_outline,
+            //         color: Colors.white,
+            //       ),
+            //       label: 'Account',
+            //     ),
+            //   ],
+            // ),
+            );
       },
     );
+  }
+
+  Widget _body() {
+    List<Widget> pages = [
+      HomePage(),
+      StorePage(),
+      CategoryPage(),
+      CustomerSigninPage()
+    ];
+    return IndexedStack(
+      index: navController.tabIndex,
+      children: pages,
+    );
+  }
+
+  Widget _bottonNavBar() {
+    return FlashyTabBar(
+        selectedIndex: navController.tabIndex,
+        animationCurve: Curves.linear,
+        backgroundColor: Colors.white,
+        items: <FlashyTabBarItem>[
+          FlashyTabBarItem(
+            icon: const Icon(Icons.apps),
+            title: const Text('Home'),
+          ),
+          FlashyTabBarItem(
+            icon: const Icon(Icons.settings),
+            title: const Text('Settings'),
+          ),
+          FlashyTabBarItem(
+            icon: const Icon(Icons.people),
+            title: const Text('Profile'),
+          ),
+          FlashyTabBarItem(
+            icon: const Icon(Icons.people),
+            title: const Text('Profile'),
+          )
+        ],
+        onItemSelected: (index) {
+          navController.onTab(index);
+        });
   }
 }
